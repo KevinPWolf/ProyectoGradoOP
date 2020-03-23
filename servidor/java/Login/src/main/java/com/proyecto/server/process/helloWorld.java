e9758c033f13a271dbf4bb36566d065ec4fd4467
anderson <afsolanoc95@gmail.com>
2020-03-22 19:30:04 -0500

    HU-12
    se añade response de tipo json al reponse de registro hello world

--- servidor/java/Login/src/main/java/com/proyecto/server/process/helloWorld.java
+++ servidor/java/Login/src/main/java/com/proyecto/server/process/helloWorld.java
@@ -9,13 +9,8 @@ public class helloWorld implements Processor {
 
 	public void process(Exchange exchange) throws Exception {
 
-		// Get input from exchange
-		//String msg = exchange.getIn().getBody(String.class);
-		// set output in exchange
-		//System.out.println("aca aca  body "+ exchange.getIn().getBody());
-		System.out.println("aca aca  body "+ exchange.getIn().getHeader("hola"));
+
 		prueba prueba= exchange.getIn().getBody(prueba.class);
-		//String msg=exchange.getIn().getHeader("hola",String.class);
 		String msg=prueba.getIdApp();
 		exchange.getOut().setBody("Hello World " + msg);
 	}
