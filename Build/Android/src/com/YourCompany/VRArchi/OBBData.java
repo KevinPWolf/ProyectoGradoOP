ce84ab5c504d602b2626e7b9028b51a76c775dca
Kevin <kwolfp12@gmail.com>
2020-05-12 23:44:38 -0500

    Arreglo errores
    Corrección error fotos y demas

--- Build/Android/src/com/YourCompany/VRArchi/OBBData.java
+++ Build/Android/src/com/YourCompany/VRArchi/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-634247614L // the length of the file in bytes
+634207319L // the length of the file in bytes
 )
 };
 };
