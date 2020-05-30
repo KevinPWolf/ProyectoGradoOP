215112c9e76bb00768d730c349510fa7535547c0
anderson <afsolanoc95@gmail.com>
2020-05-30 01:34:28 -0500

    HU-42
    se agrega plantilla html a los servicios de registro, registro inmueble, olvidar y cambiar contraseña en el mail

--- servidor/java/proyecto/UpdateApartment/src/main/java/com/proyecto/server/process/mail.java
+++ servidor/java/proyecto/UpdateApartment/src/main/java/com/proyecto/server/process/mail.java
@@ -1,50 +1,67 @@
 package com.proyecto.server.process;
 
-import java.util.Properties;
-import javax.mail.Message;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 
+
 public class mail  implements Processor {
 
+	
+	@Autowired
+	private Configuration config;
+	
+	@Autowired
+	private JavaMailSender sender;
+	
+	
 	@Override
 	public void process(Exchange exchange) throws Exception {
-		// TODO Auto-generated method stub
-		Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+
         String email = (String) exchange.getIn().getHeader("correo");
         String password = (String) exchange.getIn().getHeader("pass");
-        Session session = Session.getInstance(props,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication("ViewerRealmReal@gmail.com", "ViewerRealm69");
-                        //return new PasswordAuthentication("afsolanoc95@gmail.com", "95102508387a");
-
-                    }
-                });
-       
         try {
+
+        	MimeMessage message = sender.createMimeMessage();
+            
+            MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED,
+    				StandardCharsets.UTF_8.name());
+
+    		
+
+    		Template t = config.getTemplate("email-template.ftl");
+    		Map<String, Object> model = new HashMap<>();
+    		model.put("nombreApp", "Viewer Realm");
+    		model.put("nombreProy", "REALIDAD VIRTUAL INTERACTIVA ENFOCADO A INMOBILIARIAS MEDIANTE DISPOSITIVOS MÓVILES");
+    		model.put("mensaje",(String) exchange.getIn().getBody());
+    		model.put("correo",email);
+    		model.put("pass",password);
+			String html = FreeMarkerTemplateUtils.processTemplateIntoString(t,model);
+
+			helper.setTo(email);
+			helper.setText(html, true);
+			helper.setSubject("cambio de contraseña");
+			helper.setFrom("ViewerRealmReal@gmail.com");
+			sender.send(message);
+			
  
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("ViewerRealmReal@gmail.com"));
-            message.setRecipients(Message.RecipientType.TO,
-                    InternetAddress.parse(email));
-            message.setSubject("cambio de constraseña");
-            message.setText((String) exchange.getIn().getBody()
-            		+ " su usuario es "+email+ " contraseña: "+password);
-            Transport.send(message);
- 
-        } catch (MessagingException e) {
+        } catch (MessagingException | IOException | TemplateException  e) {
             throw new RuntimeException(e);
         }
 	}
---
+++ servidor/java/proyecto/UpdateApartment/target/classes/templates/email-template.ftl
@@ -0,0 +1,37 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Java Techie Mail</title>
+</head>
+
+<body>
+	<table width="100%" border="0" cellspacing="0" cellpadding="0">
+		<tr>
+			<td align="center" valign="top" bgcolor="#501c87"
+				style="background-color:#6060e1 ;"><br> <br>
+				<table width="600" border="0" cellspacing="0" cellpadding="0">
+					<tr>
+						<td align="center" valign="top" bgcolor="#501c87"
+							style="background-color: #75528f; font-family: Arial, Helvetica, sans-serif; font-size: 13px; color: #000000; padding: 0px 15px 10px 15px;">
+							
+							<div style="font-size: 48px; color:#60e1c1">
+								<b>Viewer Realm</b>
+							</div>
+							
+						<img src="https://raw.githubusercontent.com/KevinPWolf/ProyectoGradoOP/master/LogoViewerRealm.png" style="max-width: 50%;">
+							<div style="font-size: 24px; color: #60e1c1;">
+								<br> Viewer Realm consigue el inmueble de tus sueños<br>
+							</div>
+							<div style="color:#FFFFFF">
+								<br>${mensaje} su usuario es: <a href="#" style="text-decoration:none !important;text-underline:none; color:#FFFFFF">${correo}</a> y su nueva contraseña es: ${pass} <br>
+								<br><br><br>
+								<br> <br> <b>${nombreApp}</b><br>${nombreProy}<br>
+								<br>
+							</div>
+						</td>
+					</tr>
+				</table> <br> <br></td>
+		</tr>
+	</table>
+</body>
+</html>
